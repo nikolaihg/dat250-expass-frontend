7037424ce7117ba001247abe7ba077a2f95c71d2
nikolaihg <nikolai.gangstoe@gmail.com>
2025-09-08 13:52:01 +0200

    initial skeleton

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import CreatePollComponent from "./lib/CreatePollComponent.svelte";
+  import CreateUserComponent from "./lib/CreateUserComponent.svelte";
+  import VoteComponent from "./lib/VoteComponent.svelte";
+</script>
+
+<main>
+  <h1>Poll application:</h1>
+
+  <div class="card">
+    <CreatePollComponent />
+    <VoteComponent />
+  </div>
+  <div>
+    <CreateUserComponent />
+  </div>
+</main>
